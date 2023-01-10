6962e61318cfc5e30546d9bb5bdd2b067442f80e
galogre <irakli.galogre18@gmail.com>
2023-01-11 00:38:23 +0400

    added sass and responsive

--- js/main.js
+++ js/main.js
@@ -1,26 +1,44 @@
 const button = document.querySelector(".chart_btn");
 const popup = document.querySelector(".popup-wrapper");
 const close = document.querySelector(".popup-closer");
+
 const checkerButton = document.querySelector(".checker_btn");
 const checkerPopup = document.querySelector(".checker_popup_wrapper");
 const checkClose = document.querySelector(".checker_popup_closer");
 
-button.addEventListener("click", () => {
-  popup.style.display = "block";
+const burgerButton = document.querySelector("#burger");
+const burgerPopup = document.querySelector(".burger-wrapper");
+const burgerClose = document.querySelector(".burger-closer");
+
+console.log(button, "button");
+button?.addEventListener("click", () => {
+  if (popup) popup.style.display = "block";
 });
-close.addEventListener("click", () => {
-  popup.style.display = "none";
+close?.addEventListener("click", () => {
+  if (popup) popup.style.display = "none";
 });
-popup.addEventListener("click", () => {
-  popup.style.display = "none";
+popup?.addEventListener("click", () => {
+  if (popup) popup.style.display = "none";
 });
+
 //checker
-checkerButton.addEventListener("click", () => {
+checkerButton?.addEventListener("click", () => {
   checkerPopup.style.display = "block";
 });
-checkClose.addEventListener("click", () => {
+checkClose?.addEventListener("click", () => {
   checkerPopup.style.display = "none";
 });
-checkerPopup.addEventListener("click", () => {
+checkerPopup?.addEventListener("click", () => {
   checkerPopup.style.display = "none";
+});
+
+//burger
+burgerButton?.addEventListener("click", () => {
+  burgerPopup.style.display = "block";
+});
+burgerClose?.addEventListener("click", () => {
+  burgerPopup.style.display = "none";
+});
+burgerPopup?.addEventListener("click", () => {
+  burgerPopup.style.display = "none";
 });
